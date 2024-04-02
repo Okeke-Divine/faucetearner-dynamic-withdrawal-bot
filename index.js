a6f45af71db5c3ceff605b33a69a2903db78f471
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-02 14:48:43 +0100

    Incomplete update

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
-const express = require("express")
-const { withdrawLogic } = require("./withdrawLogic")
+const express = require("express");
+const { withdrawLogic } = require("./withdrawLogic");
+const dotenv = require("dotenv");
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
-app.get("/", (req, res) => {
-  // const { uname, pswd } = req.query;
-  // if (uname == undefined || pswd == undefined) {
-  //   res.send('Params error! Kill the script');
-  //   console.log('Params error! Kill the script');
-  // } else {
-  //   res.send('Intialising withdrawal bot for uname:' + uname + ' pswd:' + pswd)
-  //   withdrawLogic(res, uname, pswd);
-  // }
-})
+dotenv.config();
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`)
-})
+app.get("/manual-call", (req, res) => {
+  const { uname, pswd } = req.query;
+  if (uname == undefined || pswd == undefined) {
+    res.send('Params error! Kill the script');
+    console.log('Params error! Kill the script');
+  } else {
+    res.send('Initializing withdrawal bot for uname:' + uname + ' pswd:' + pswd);
+    withdrawLogic(res, uname, pswd);
+  }
+});
+
+(async () => {
+  console.log(`Listening on port ${PORT}`);
+  
+  const users = process.env.USERS;
+  
+  if (!users) {
+    console.log('No users found in .env file');
+    return;
+  }
+  
+  const parsedUsers = JSON.parse(users);
+  
+  if (!Array.isArray(parsedUsers)) {
+    console.log('Invalid format for USERS in .env file');
+    return;
+  }
+  
+  for (const user of parsedUsers) {
+    const { name: uname, password: pswd } = user;
+    
+    if (!uname || !pswd) {
+      console.log('Invalid user data found in .env file:', user);
+      continue;
+    }
+    
+    console.log(`Processing withdrawal for user: ${uname}`);
+    await withdrawLogic(null, uname, pswd);
+    console.log(`Withdrawal completed for user: ${uname}`);
+  }
+  
+  console.log('All withdrawals completed');
+})();
+
+app.listen(PORT);
